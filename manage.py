63e75ad6d1ed93c861430a5edfd0956e24505d9b
Froggy <froggy.happy.f@gmail.com>
2021-10-04 17:53:32 +0300

    Отредактировал settings и починил runserver

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Land_of_the_Imagineers.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Land_of_the_Imagineers.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
