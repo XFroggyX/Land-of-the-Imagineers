525dd0c23a65a0b31c64a8648ce3073290958a8a
Savage <savagetaegis@gmail.com>
2022-01-15 15:33:18 +0300

    Добавлена возможность получения текущего пользователя

--- main/urls.py
+++ main/urls.py
@@ -13,20 +13,21 @@ router.register(r'town', TownViewSet, 'town')
 router.register(r'struct', StructTownViewSet, 'town')
 router.register(r'user', UserViewSet, 'town')
 router.register(r'user_list', UserListViewSet, 'town')
-#router.register('api/list', towns_list.as_view(), 'town_list'),
-#router.register('api/list/<int:pk>', snippet_detail.as_view(), 'town_detail')
+# router.register('api/list', towns_list.as_view(), 'town_list'),
+# router.register('api/list/<int:pk>', snippet_detail.as_view(), 'town_detail')
 
 
 urlpatterns = [
     url(r'login/', views.login_page),
     url(r'main/', include('field_game.urls')),
     url(r'sign_up/', views.sign_up),
-    #url('api/user_list', users_towns_list),
+    # url('api/user_list', users_towns_list),
     url('api/list', towns_list),
     url('api/list/<int:pk>', snippet_detail),
-    url('api/', include(router.urls))
+    url('api/', include(router.urls)),
+    url('api/current_user', views.current_user)
     # url('api/struct/', user_town_list),
     # url('api/struct/<int:pk>', user_town_detail),
 ]
 
-#urlpatterns += router.urls
+# urlpatterns += router.urls
